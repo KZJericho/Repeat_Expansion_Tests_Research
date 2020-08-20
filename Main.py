3ceac68b2b81c612312b0416a3eb4b5cf7847510
Kevin Zhu <KZJericho@gmail.com>
2020-08-20 11:20:11 -0400

    Removed Print Statements

--- Main.py
+++ Main.py
@@ -1,7 +1,3 @@
-def myPrint(*args):
-    if False:
-        print(*args)
-    
 def my_mod(dividend, divisor):
     mod = dividend%divisor
     if mod < 0:
@@ -40,28 +36,20 @@ def find_best_pattern(txt, pat):
     for i in range(pattern_length):
         pattern_hash = (base*pattern_hash + ord(pat[i]))%prime
         current_txt_hash = my_mod((base*current_txt_hash + ord(txt[i])),prime)
-    myPrint("first_hash=", current_txt_hash)
-    myPrint("PATTERNHASH=", pattern_hash)
 
     i = 0
     #rolling hash
     while i < (len(txt)):
         number_of_letters_rolling_over = 0 
         current_chunk = txt[i:i+pattern_length] 
-        myPrint("i=", i)      
-        myPrint("current_chunk=", current_chunk)
-        myPrint("current_txt_hash=", current_txt_hash)
-        myPrint("pattern_hash=", pattern_hash)
         if current_txt_hash == pattern_hash and current_chunk == pat:
                 current_length += 1  
-                myPrint("current_length=", current_length) 
                 current_location = i 
 
                 number_of_letters_rolling_over = len(pat)
         
         #record best lengths
         else:
-            myPrint("here1")
             if current_length > best_length:
                 best_length = current_length
                 current_best_location = current_location - (len(pat))*((current_length)-1)
@@ -71,17 +59,10 @@ def find_best_pattern(txt, pat):
         
         #re-hash
         if i + number_of_letters_rolling_over < text_length - pattern_length:
-            myPrint("here2")
-            myPrint("current_txt_hash=", current_txt_hash)
-            myPrint("ordvalue1=", ord(txt[i]))
-            myPrint("exponent=", exponent)
-            myPrint("ordvalue2=", ord(txt[i+pattern_length]))
             for j in range(number_of_letters_rolling_over):
                 current_txt_hash = my_mod((base*(current_txt_hash-ord(txt[i])*exponent) + ord(txt[i+pattern_length])),prime)
                 i += 1
                 
-
-            myPrint("newhash=", current_txt_hash)
             
             #in case of negative hash value
             if current_txt_hash < 0:
@@ -109,7 +90,7 @@ def run_best_pattern_on_genome(file, pattern):
     print(find_best_pattern(pattern, genome))
 
     
-def run_main_for_HTT():
+def run_main():
     genomes = ['Test_1_NM_002111.6.txt'] #add all genome names here
     pattern = "CAG"
     for file in genomes: 
@@ -117,4 +98,4 @@ def run_main_for_HTT():
         print("For file %s, the pattern %s repeats %d times starting at index %d" % (file, pattern, repetitions, location))
 
 if __name__ == '__main__':
-    run_main_for_HTT()
+    run_main()
