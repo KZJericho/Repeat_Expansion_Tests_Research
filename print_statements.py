df3caa8e83dfdeede84472727ff8ae31654134f3
Kevin Zhu <KZJericho@gmail.com>
2020-08-20 12:55:45 -0400

    fixed bug with single patterns, added print as separate file

--- print_statements.py
+++ print_statements.py
@@ -1,3 +1,7 @@
+def myPrint(*args):
+    if True:
+        print(*args)
+        
 def my_mod(dividend, divisor):
     mod = dividend%divisor
     if mod < 0:
@@ -32,24 +36,33 @@ def find_best_pattern(txt, pat):
     for i in range(pattern_length - 1):
         exponent = (base**(pattern_length-1))%prime
         
+        
     #calculate first hash and pattern hash    
     for i in range(pattern_length):
         pattern_hash = (base*pattern_hash + ord(pat[i]))%prime
         current_txt_hash = my_mod((base*current_txt_hash + ord(txt[i])),prime)
+        myPrint("first_hash=", current_txt_hash)
+        myPrint("PATTERNHASH=", pattern_hash)
 
     i = 0
     #rolling hash
     while i < (len(txt) - len(pat) + 1):
         number_of_letters_rolling_over = 0 
         current_chunk = txt[i:i+pattern_length] 
+        myPrint("i=", i)      
+        myPrint("current_chunk=", current_chunk)
+        myPrint("current_txt_hash=", current_txt_hash)
+        myPrint("pattern_hash=", pattern_hash)
         if current_txt_hash == pattern_hash and current_chunk == pat:
                 current_length += 1  
+                myPrint("current_length=", current_length)
                 current_location = i 
 
                 number_of_letters_rolling_over = len(pat)
         
         #record best lengths
         else:
+            myPrint("here1")
             if current_length > best_length:
                 best_length = current_length
                 current_best_location = current_location - (len(pat))*((current_length)-1)
@@ -58,11 +71,17 @@ def find_best_pattern(txt, pat):
             number_of_letters_rolling_over = 1 
         
         #re-hash
-        if i + number_of_letters_rolling_over < text_length - pattern_length:
+        if i + number_of_letters_rolling_over <= text_length - pattern_length:
+            myPrint("here2")
+            myPrint("current_txt_hash=", current_txt_hash)
+            myPrint("ordvalue1=", ord(txt[i]))
+            myPrint("exponent=", exponent)
+            myPrint("ordvalue2=", ord(txt[i+pattern_length]))
             for j in range(number_of_letters_rolling_over):
                 current_txt_hash = my_mod((base*(current_txt_hash-ord(txt[i])*exponent) + ord(txt[i+pattern_length])),prime)
                 i += 1
                 
+            myPrint("newhash=", current_txt_hash)
             
             #in case of negative hash value
             if current_txt_hash < 0:
@@ -78,24 +97,4 @@ def find_best_pattern(txt, pat):
     
     return (best_length, current_best_location)
 
-def open_genome_file_as_string(filename): 
-    file = open(filename, 'r')
-    lines = file.readlines()
-    result = "".join(lines) 
-    return result 
 
-def run_best_pattern_on_genome(file, pattern):
-    genome = open_genome_file_as_string(file)
-    print(find_best_pattern(pattern, genome))
-
-    
-def run_main():
-    genomes = [''] #add all genome names here
-    pattern = "" #add pattern here
-    for file in genomes: 
-        repetitions, location = run_best_pattern_on_genome(file, pattern)
-        print("For file %s, the pattern %s repeats %d times starting at index %d" % (file, pattern, repetitions, location))
-
-if __name__ == '__main__':
-    run_main()
-'''
